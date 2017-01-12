dfde17811d33c86f80a523f2e1fa228a8e3e35ee
Dan Reeves <heydanreeves@gmail.com>
2017-01-12 22:17:14 +0000

    refactor, initial implementation of #5

--- prettier.py
+++ prettier.py
@@ -1,4 +1,4 @@
-from subprocess import check_output, CalledProcessError
+from subprocess import Popen, check_output, CalledProcessError, PIPE
 from sublime import Region, load_settings
 import sublime_plugin
 
@@ -11,27 +11,50 @@ setting_keys = [
     {'key': 'bracketSpacing', 'option': '--bracket-spacing'},
 ]
 settings = load_settings('Prettier.sublime-settings')
+options = []
+
+# convert the sublime-settings into cli args
+for setting in setting_keys:
+    opt = settings.get(setting['key'], False)
+    if opt:
+        options.append(setting['option'])
+        if not isinstance(opt, bool):
+            options.append(str(opt))
 
 
+def do_replace(edit, view, region, replacement):
+    encoding = view.encoding() if view.encoding() != 'Undefined' else 'utf-8'
+    view.replace(edit, region, replacement.decode(encoding))
+
+
+def report_error(returncode, err=None):
+    print('prettier failed with error code ' + str(returncode))
+    if err:
+        print(err)
+
+
+# Run prettier an a file
 class PrettierCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        options = []
-        for setting in setting_keys:
-            opt = settings.get(setting['key'], False)
-            if opt:
-                options.append(setting['option'])
-                if not isinstance(opt, bool):
-                    options.append(str(opt))
-
-        view = self.view
-        view_region = Region(0, view.size())
-        file_path = view.file_name()
-        file_encoding = view.encoding() if view.encoding() != 'Undefined' else 'utf-8'
-        command = ['prettier'] + options + [file_path]
+        path = self.view.file_name()
+        command = ['prettier'] + options + [path]
+        region = Region(0, self.view.size())
         try:
             prettier_output = check_output(command)
-            view.replace(edit, view_region, prettier_output.decode(file_encoding))
+            do_replace(edit, self.view, region, prettier_output)
         except CalledProcessError as e:
-            print('prettier failed with ' + str(e.returncode))
-            if e.output:
-                print(e.output)
+            report_error(e.returncode, e.output)
+
+
+# Run prettier on a selection in a file
+class PrettierSelectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        command = ['prettier'] + options + ['--stdin']
+        for region in self.view.sel():
+            text = self.view.substr(region)
+            proc = Popen(command, stdin=PIPE, stderr=PIPE, stdout=PIPE)
+            prettier_output, err = proc.communicate(str.encode(text))
+            if err or proc.returncode != 0:
+                report_error(proc.returncode, err)
+            else:
+                do_replace(edit, self.view, region, prettier_output)
