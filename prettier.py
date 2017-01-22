407a0aad7a9889fa9f27e6dd66f5a91681334cbf
Adrien Antoine <adriantoine@gmail.com>
2017-01-22 13:58:31 +0000

    Fixed autosave by changing the way the prettier command works

--- prettier.py
+++ prettier.py
@@ -1,6 +1,9 @@
-from subprocess import Popen, check_output, CalledProcessError, PIPE
+import subprocess
+import platform
 from sublime import Region, load_settings
 import sublime_plugin
+
+IS_WINDOWS = platform.system() == 'Windows'
 
 setting_keys = [
     {'key': 'printWidth', 'option': '--print-width'},
@@ -32,18 +35,30 @@ def report_error(returncode, err=None):
     if err:
         print(err)
 
+# Calls prettier on the given region and replaces code
+def prettify_code(edit, view, region):
+    command = ['prettier'] + options + ['--stdin']
+    code = view.substr(region)
+    proc = subprocess.Popen(
+        command,
+        stdin=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        shell=IS_WINDOWS
+    )
+    prettier_output, err = proc.communicate(str.encode(code))
+
+    if err or proc.returncode != 0:
+        report_error(proc.returncode, err)
+    else:
+        do_replace(edit, view, region, prettier_output)
+
 
 # Run prettier an a file
 class PrettierCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        path = self.view.file_name()
-        command = ['prettier'] + options + [path]
         region = Region(0, self.view.size())
-        try:
-            prettier_output = check_output(command)
-            do_replace(edit, self.view, region, prettier_output)
-        except CalledProcessError as e:
-            report_error(e.returncode, e.output)
+        prettify_code(edit, self.view, region)
 
 
 # Run prettier on a selection in a file
@@ -52,17 +67,13 @@ class PrettierSelectionCommand(sublime_plugin.TextCommand):
         command = ['prettier'] + options + ['--stdin']
         for region in self.view.sel():
             text = self.view.substr(region)
-            proc = Popen(command, stdin=PIPE, stderr=PIPE, stdout=PIPE)
-            prettier_output, err = proc.communicate(str.encode(text))
-            if err or proc.returncode != 0:
-                report_error(proc.returncode, err)
-            else:
-                do_replace(edit, self.view, region, prettier_output)
+            prettify_code(edit, self.view, region)
+
 
 # Prettier event listeners
 class PrettierListener(sublime_plugin.EventListener):
     def on_pre_save(self, view):
-        # Run the prettier sublime command on save if autoformat is not unset
+        # Run the prettier sublime command on save if autoformat is set to true
         autoformat = settings.get('autoformat')
-        if autoformat != False:
+        if autoformat == True:
             view.run_command('prettier')
