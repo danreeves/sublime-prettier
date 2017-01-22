bd6746e4bb9e227edc144ec9917b5fe8435ed5b9
Adrien Antoine <adriantoine@gmail.com>
2017-01-22 14:41:52 +0000

    Implemented autoformatting on save (#9)
    * Implemented autoformatting on save
    * Refactor format file and format selection to use the same implementation
    * Update README
    
    Thanks @adriantoine!

--- prettier.py
+++ prettier.py
@@ -36,37 +36,43 @@ def report_error(returncode, err=None):
         print(err)
 
 
+# Calls prettier on the given region and replaces code
+def prettify_code(edit, view, region):
+    command = ['prettier'] + options + ['--stdin']
+    code = view.substr(region)
+    proc = subprocess.Popen(
+        command,
+        stdin=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        shell=IS_WINDOWS
+    )
+    prettier_output, err = proc.communicate(str.encode(code))
+
+    if err or proc.returncode != 0:
+        report_error(proc.returncode, err)
+    else:
+        do_replace(edit, view, region, prettier_output)
+
+
 # Run prettier an a file
 class PrettierCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        path = self.view.file_name()
-        command = ['prettier'] + options + [path]
         region = Region(0, self.view.size())
-        try:
-            prettier_output = subprocess.check_output(
-                command,
-                shell=IS_WINDOWS
-            )
-            do_replace(edit, self.view, region, prettier_output)
-        except subprocess.CalledProcessError as e:
-            report_error(e.returncode, e.output)
+        prettify_code(edit, self.view, region)
 
 
 # Run prettier on a selection in a file
 class PrettierSelectionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        command = ['prettier'] + options + ['--stdin']
         for region in self.view.sel():
-            text = self.view.substr(region)
-            proc = subprocess.Popen(
-                command,
-                stdin=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                shell=IS_WINDOWS
-            )
-            prettier_output, err = proc.communicate(str.encode(text))
-            if err or proc.returncode != 0:
-                report_error(proc.returncode, err)
-            else:
-                do_replace(edit, self.view, region, prettier_output)
+            prettify_code(edit, self.view, region)
+
+
+# Prettier event listeners
+class PrettierListener(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        # Run the prettier sublime command on save if autoformat is set to true
+        autoformat = settings.get('autoformat')
+        if autoformat is True:
+            view.run_command('prettier')
